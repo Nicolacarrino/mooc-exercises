1b158f13901fc9420de84fd3276331b5e9fbad14
Nicolacarrino <carrino.nicola1996@gmail.com>
2021-07-31 12:22:50 +0200

    Solved Visual Lane and Collision

--- collision/collision_checker/collision_checker.py
+++ collision/collision_checker/collision_checker.py
@@ -12,7 +12,11 @@ from dt_protocols import (
     Rectangle,
 )
 
+import shapely.geometry
+
 __all__ = ["CollisionChecker"]
+
+PRINT = False
 
 
 class CollisionChecker:
@@ -39,12 +43,12 @@ def check_collision(
     # This is just some code to get you started, but you don't have to follow it exactly
 
     # start by rototranslating the robot parts by the robot pose
-    rototranslated_robot: List[PlacedPrimitive] = []  #
+    rototranslated_robot: List[PlacedPrimitive] = [PlacedPrimitive(robot_pose, x.primitive) for x in robot_body ]
 
     collided = check_collision_list(rototranslated_robot, Wcoll)
 
     # return a random choice
-    return random.uniform(0, 1) > 0.5
+    return collided
 
 
 def check_collision_list(A: List[PlacedPrimitive], B: List[PlacedPrimitive]) -> bool:
@@ -59,11 +63,54 @@ def check_collision_list(A: List[PlacedPrimitive], B: List[PlacedPrimitive]) -> 
 
 def check_collision_shape(a: PlacedPrimitive, b: PlacedPrimitive) -> bool:
     # This is just some code to get you started, but you don't have to follow it exactly
+    if PRINT:
+        print(a)
+        print(a.primitive)
+    a_shap = shapely.geometry.Point(0, 0).buffer(1)
+    b_shap = shapely.geometry.Point(0, 0).buffer(1)
 
-    if isinstance(a, Circle) and isinstance(b, Circle):
-        ...
-    if isinstance(a, Circle) and isinstance(b, Rectangle):
-        ...
+
+    if isinstance(a.primitive, Circle):
+        a_shap = shapely.geometry.Point(a.pose.x, a.pose.y).buffer(a.primitive.radius)
+        if PRINT:
+            print("a Circle")
+
+    if isinstance(b.primitive, Circle):
+        b_shap = shapely.geometry.Point(b.pose.x, b.pose.y).buffer(b.primitive.radius)
+        if PRINT:
+            print("b Circle")
+
+    if isinstance(a.primitive, Rectangle):
+        a_shap = get_shapely_rect(a)
+        if PRINT:
+            print("a Rectangle")
+
+    if isinstance(b.primitive, Rectangle):
+        b_shap = get_shapely_rect(b)
+        if PRINT:
+            print("b Rectangle")
+
+    
+    collision = a_shap.intersects(b_shap)
+    if PRINT:
+        print("collision: ",collision)
+        print()
+        print()
+
+    # if isinstance(a, Circle) and isinstance(b, Circle):
+    #     ...
+    # if isinstance(a, Circle) and isinstance(b, Rectangle):
     # for now let's return a random guess
 
-    return ...
+    return collision
+
+def get_shapely_rect(pprim: PlacedPrimitive):
+    rect = pprim.primitive
+
+    origin = (pprim.pose.x, pprim.pose.y)
+
+    shapely_rect = shapely.geometry.box(rect.xmin, rect.ymin, rect.xmax, rect.ymax)
+    shapely_rect = shapely.affinity.translate(shapely_rect, xoff=pprim.pose.x, yoff=pprim.pose.y, zoff=0.0)
+    shapely_rect = shapely.affinity.rotate(shapely_rect, pprim.pose.theta_deg, origin=origin)
+
+    return shapely_rect
--- visual_lane_servoing/solution/src/visual_lane_servoing/src/visual_servoing_activity.py
+++ visual_lane_servoing/solution/src/visual_lane_servoing/src/visual_servoing_activity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[92]:
+# In[16]:
 
 
 # The function written in this cell will actually be ran on your robot (sim or real). 
@@ -21,26 +21,27 @@ def get_steer_matrix_left_lane_markings(shape):
             steer_matrix_left_lane: The steering (angular rate) matrix for Braitenberg-like control 
                                     using the masked left lane markings (numpy.ndarray)
     """
-    x = np.linspace(0, 1, shape[1]//2)
-    y = np.linspace(0, 1, shape[0]//2)
-    
-    a, b = np.meshgrid(x,y)
-    gradient = (a+b)/2
-    
     steer_matrix_left_lane = np.zeros(shape=shape, dtype="float32")  # write your function instead of this one
-    steer_matrix_left_lane[math.ceil(steer_matrix_left_lane.shape[0]/2):, :gradient.shape[1]] = gradient
-    
-    x = np.linspace(-1, 0, shape[1]//2)
-    y = np.linspace(0, -1, shape[0]//2)
+
+    x = np.linspace(0, -1, shape[1]//2)
+    y = np.linspace(0, -1, shape[0])
     
     a, b = np.meshgrid(x,y)
     gradient = (a+b)/2
+    
+    steer_matrix_left_lane[:, :gradient.shape[1]] = gradient
+    
+#     x = np.linspace(1, 0, shape[1]//2)
+#     y = np.linspace(0, 1, shape[0]//2)
+    
+#     a, b = np.meshgrid(x,y)
+#     gradient = (a+b)/2
 
-    steer_matrix_left_lane[math.ceil(steer_matrix_left_lane.shape[0]/2):, math.ceil(steer_matrix_left_lane.shape[1]/2):] = gradient
+#     steer_matrix_left_lane[math.ceil(steer_matrix_left_lane.shape[0]/2):, math.ceil(steer_matrix_left_lane.shape[1]/2):] = gradient
 
     return steer_matrix_left_lane
 
-# In[94]:
+# In[56]:
 
 
 # The function written in this cell will actually be ran on your robot (sim or real). 
@@ -56,26 +57,27 @@ def get_steer_matrix_right_lane_markings(shape):
             steer_matrix_right_lane: The steering (angular rate) matrix for Braitenberg-like control 
                                      using the masked right lane markings (numpy.ndarray)
     """
-    x = np.linspace(0, -1, shape[1]//2)
-    y = np.linspace(0, -1, shape[0]//2)
-    
-    a, b = np.meshgrid(x,y)
-    gradient = (a+b)/2
-    
     steer_matrix_right_lane = np.zeros(shape=shape, dtype="float32")  # write your function instead of this one
-    steer_matrix_right_lane[math.ceil(steer_matrix_right_lane.shape[0]/2):, :gradient.shape[1]] = gradient
+
+#     x = np.linspace(0, -1, shape[1]//2)
+#     y = np.linspace(0, -1, shape[0]//2)
     
-    x = np.linspace(1, 0, shape[1]//2)
-    y = np.linspace(0, 1, shape[0]//2)
+#     a, b = np.meshgrid(x,y)
+#     gradient = (a+b)/2
+    
+#     steer_matrix_right_lane[math.ceil(steer_matrix_right_lane.shape[0]/2):, :gradient.shape[1]] = gradient
+    
+    x = np.linspace(3.3, 0, shape[1]//2)
+    y = np.linspace(0, 3.3, shape[0])
     
     a, b = np.meshgrid(x,y)
     gradient = (a+b)/2
     
-    steer_matrix_right_lane[math.ceil(steer_matrix_right_lane.shape[0]/2):, math.ceil(steer_matrix_right_lane.shape[1]/2):] = gradient
+    steer_matrix_right_lane[:, math.ceil(steer_matrix_right_lane.shape[1]/2):] = gradient
     
     return steer_matrix_right_lane
 
-# In[162]:
+# In[58]:
 
 
 # The function written in this cell will actually be ran on your robot (sim or real). 
@@ -98,6 +100,7 @@ def detect_lane_markings(image):
     h, w, _ = image.shape
     
     img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    img = cv2.GaussianBlur(img,(0,0), 1)
     
     sobelx = cv2.Sobel(img,cv2.CV_64F,1,0)
     sobely = cv2.Sobel(img,cv2.CV_64F,0,1)
@@ -116,32 +119,45 @@ def detect_lane_markings(image):
     mask_sobely_pos = (sobely > 0)
     mask_sobely_neg = (sobely < 0)
     
-    white_lower_hsv = np.array([50, 0, 100])         # CHANGE ME
-    white_upper_hsv = np.array([179, 255, 255])      # CHANGE ME
-    yellow_lower_hsv = np.array([15, 100, 110])      # CHANGE ME
-    yellow_upper_hsv = np.array([30, 255, 255])      # CHANGE ME
+    white_lower_hsv = np.array([0, 0, 160])         # CHANGE ME
+    white_upper_hsv = np.array([179, 50, 255])      # CHANGE ME
+    yellow_lower_hsv = np.array([15, 60, 110])      # CHANGE ME
+    yellow_upper_hsv = np.array([35, 255, 255])      # CHANGE ME
     
     imghsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         
     mask_white = cv2.inRange(imghsv, white_lower_hsv, white_upper_hsv)
     mask_yellow = cv2.inRange(imghsv, yellow_lower_hsv, yellow_upper_hsv)
     
-    mask_left_edge = mask_left * mask_mag * mask_sobelx_neg * mask_sobely_neg * mask_yellow * Gmag
-    mask_right_edge = mask_right * mask_mag * mask_sobelx_pos * mask_sobely_neg * mask_white * Gmag
+    mask_left_edge = mask_left * mask_mag * mask_sobelx_neg * mask_sobely_neg * mask_yellow
+    mask_right_edge = mask_right * mask_mag * mask_sobelx_pos * mask_sobely_neg * mask_white
     
-    max_val_left = np.max(mask_left_edge)
-    max_val_right = np.max(mask_right_edge)
+#     max_left = np.max(mask_left_edge)
+#     max_right = np.max(mask_right_edge)
+#     mask_left_edge[mask_left_edge > 0.6*max_left] = 0
+#     mask_right_edge[mask_right_edge > 0.6*max_right] = 0
     
-    mask_left_edge[mask_left_edge > 0] = 1
-    mask_right_edge[mask_right_edge > 0] = 1
+#     mask_left_edge[mask_left_edge > 0] = 1.0
+#     mask_right_edge[mask_right_edge > 0] = 1.0
+#     print(np.sum(mask_left_edge > 0))
     
-#     thresh = 0.2
-#     mask_left_edge[mask_left_edge <= thresh*max_val_left] = 0
-#     mask_right_edge[mask_right_edge <= thresh*max_val_right] = 0
-#     mask_left_edge[mask_left_edge > thresh*max_val_left] = 1
-#     mask_right_edge[mask_right_edge > thresh*max_val_right] = 1
-#     fig = plt.figure(figsize = (10,5))
-#     ax1 = fig.add_subplot(1,2,1)
-#     ax1.hist(np.extract(mask_left_edge, Gdir).flatten(), bins=30)
+    
+#     mask_left_edge[mask_left_edge > 0] = 1.0
+#     mask_right_edge[mask_right_edge > 0] = 1.0
+
+    # Debug    
+    STEER_LEFT_LM = get_steer_matrix_left_lane_markings((h,w))
+    STEER_RIGHT_LM = get_steer_matrix_right_lane_markings((h,w))
+    steering  = np.sum( STEER_LEFT_LM * mask_left_edge) + np.sum( STEER_RIGHT_LM * mask_right_edge)
+    print("my steering: ",steering)
+    print("left: ",np.sum( STEER_LEFT_LM * mask_left_edge))
+    print("right: ",np.sum( STEER_RIGHT_LM * mask_right_edge))
+    
+#     print(mask_left_edge[mask_left_edge>0])
+#     print(mask_right_edge[mask_right_edge>0])
+    
+    
     
     return (mask_left_edge, mask_right_edge)
+
+# UnitTestDLM(detect_lane_markings)
